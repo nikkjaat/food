4073706f73f3c2b63dcfa1366c6d4c63ef7d8a38
Nikhil <nikhiljaat330@gmail.com>
2024-05-07 14:18:39 +0530

    changes

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,25 +1,17 @@
-// const allowedOrigins = [
-//   "https://nikhil-food-lmgsp6lut-nikhils-projects-2570a626.vercel.app",
-//   "https://aesthetic-toffee-df3bb9.netlify.app",
-//   ,
-// ];
-
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true,
-//   optionsSuccessStatus: 200,
-// };
-
-// module.exports = corsOptions;
+const allowedOrigins = [
+  "https://nikhil-food-lmgsp6lut-nikhils-projects-2570a626.vercel.app",
+  "https://aesthetic-toffee-df3bb9.netlify.app",
+  ,
+];
 
 const corsOptions = {
-  origin: "*", // Allow requests from any origin
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   optionsSuccessStatus: 200,
 };
