a062532f0ff1b989fe570703f8e46e2006125bc3
Nikhil <nikhiljaat330@gmail.com>
2024-07-11 21:10:25 +0530

    update

--- Controllers/Payment.js
+++ Controllers/Payment.js
@@ -1,36 +1,104 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const User = require("../models/User");
+
+// exports.postCheckout = async (req, res, next) => {
+// const addressId = req.body.addressId;
+// const user = await req.user.populate("cart.productId");
+// console.log(user, addressId);
+// const products = user.cart;
+// let total = 0;
+// products.forEach((p) => (total += p.quantity * p.productId.price));
+// // Fetch user address from your database or req.body
+// const address = user.address.find(
+//   (addr) => addr._id.toString() === addressId
+// );
+// if (!address) {
+//   return res.status(400).json({ message: "Address not found" });
+// }
+// const customer = await stripe.customers.create({
+//   name: `${user.firstName} ${user.lastName}`, // Assuming you have firstName and lastName in your user model
+//   email: user.email, // Assuming you have email in your user model
+//   address: {
+//     line1: address.line1,
+//     line2: address.line2,
+//     city: address.city,
+//     state: address.state,
+//     postal_code: address.postalCode,
+//     country: address.country,
+//   },
+//   metadata: {
+//     addressId: addressId,
+//     userId: req.user._id.toString(),
+//   },
+// });
+// try {
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: total * 100, // Convert to smallest currency unit (cents, paise, etc.)
+//     currency: "INR",
+//     description: "Payment for purchase from Stripe",
+//     customer: customer.id,
+//     automatic_payment_methods: {
+//       enabled: true,
+//     },
+//   });
+//   res.send({
+//     clientSecret: paymentIntent.client_secret,
+//   });
+// } catch (err) {
+//   res.status(500).json({ message: err.message });
+// }
+// };
+
+const Product = require("../models/Product");
 const User = require("../models/User");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-exports.postCheckout = async (req, res, next) => {
-  const addressId = req.body.addressId;
-
-  const user = await req.user.populate("cart.productId");
-  const products = user.cart;
-  let total = 0;
-  products.forEach((p) => (total += p.quantity * p.productId.price));
-  const customer = await stripe.customers.create({
-    metadata: {
-      addressId: addressId,
-      userId: req.user._id.toString(),
-    },
-  });
+exports.postCheckout = async (req, res) => {
+  const { addressId, productId, userId, quantity } = req.body;
 
   try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: total,
+    const product = await Product.findById(productId);
+    // console.log(product);
+
+    const user = await User.findById(userId);
+    const address = await user.getSingleAddress(addressId);
+
+    const price = await stripe.prices.create({
+      unit_amount: product.price * 100,
       currency: "INR",
-      description: "Payment for purchase from Stripe",
-      customer: customer.id,
-      // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-      automatic_payment_methods: {
-        enabled: true,
+      product_data: {
+        name: product.name,
       },
     });
 
-    res.send({
-      clientSecret: paymentIntent.client_secret,
+    const customer = await stripe.customers.create({
+      name: address.name,
+
+      address: {
+        line1: address.street,
+        city: address.city,
+        state: address.state,
+        postal_code: address.pincode,
+        country: "IN",
+      },
     });
-  } catch (err) {
-    res.status(500).json({ message: err });
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: price.id,
+          quantity: quantity,
+        },
+      ],
+      customer: customer.id,
+      mode: "payment",
+      success_url: `http://localhost:3000/`,
+      cancel_url: `http://localhost:3000/cancel`,
+    });
+
+    res.json({ url: session.url });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 //CORS POLICY
 const cors = require("cors");
